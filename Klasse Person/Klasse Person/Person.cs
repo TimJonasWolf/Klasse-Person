68bece5651814fcb7083efb9805acd597d17c5ac
TimJonasWolf <132747647+TimJonasWolf@users.noreply.github.com>
2024-01-23 22:39:24 +0100

    Add files via upload

---
+++ Klasse Person/Klasse Person/Person.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasse_Person
+{
+	internal class Person
+	{
+		public string name { get; set; }
+
+		public string vorname { get; set; }
+
+		public string adresse { get; set; }
+		public int alter { get; set; }
+		public string email { get; set; }
+
+		public Person() 
+		{
+			Eingabe(); 
+		}
+
+		//public Person(string N,string v, string A, int al, string EM) 
+		//{
+		//	name= N;
+		//	vorname= v;
+		//	adresse= A;
+		//	alter= al;
+		//	email= EM;
+		//}	
+
+		public void Eingabe() 
+		{
+			try 
+			{
+				Console.WriteLine("\nGeben sie ihren Namen ein : ");
+				name = Console.ReadLine();
+				Console.WriteLine("\nGeben sie ihren Vornamen ein : ");
+				vorname = Console.ReadLine();
+				Console.WriteLine("\nGeben sie ihre Adresse ein : ");
+				adresse = Console.ReadLine();
+				Console.WriteLine("\nGeben sie ihr Alter ein : ");
+				alter = Convert.ToInt32(Console.ReadLine());
+				Console.WriteLine("\nGeben sie ihre E-Mail ein : ");
+				email = Console.ReadLine();
+				Console.WriteLine("\n-----------------------------------------------\n");
+
+
+
+
+
+
+			}
+			catch(Exception ex) 
+			{
+				Console.WriteLine(ex.Message);
+			}
+		}
+		public void Ausgabe() 
+		{
+			Console.WriteLine($"Name	: {name}\n" +
+							  $"Vorname : {vorname}\n" +
+							  $"Adresse : {adresse}\n" +
+							  $"Alter	: {alter}\n" +
+							  $"E-Mail	: {email}\n" );
+		}
+
+		public void GeburtsjahrAusgabe() 
+		{ 
+			Console.WriteLine($" Ihr Geburtsjahr ist : {2023-alter}");
+
+
+		}
+	}
+}
---
+++ Klasse Person/Klasse Person/Program.cs
@@ -0,0 +1,51 @@
+﻿
+namespace Klasse_Person
+{
+	class Prog 
+	{
+		//static void Main(string[] args) 
+		//{
+		//	Person person = new Person();
+
+		//	//person.name = "Batman";
+		//	//person.vorname = "Bat";
+
+		//	//Console.WriteLine($"Name : {person.name} Vorname : {person.vorname}");
+
+		//	person.Eingabe();
+		//	person.Ausgabe();
+		//	person.GeburtsjahrAusgabe();
+
+		//	Console.ReadKey();
+		//}
+
+		//----------------------------------------------------------------------------------------------------------------------------------
+
+		static void Main(string[] args)
+		{
+			Person[] person_array = new Person[2];
+
+			for(int i = 0; i < person_array.Length; i++)
+			{
+				Person person = new Person();
+				//person.Eingabe();
+				person_array[i] = person;
+
+			}
+
+			foreach(var p in person_array)
+			{
+				p.Ausgabe();
+				Console.WriteLine("\n---------------------------\n");
+				p.GeburtsjahrAusgabe();
+				Console.WriteLine("\n---------------------------\n");
+			}
+
+			
+
+
+			Console.ReadKey();
+		}
+	}
+
+}
